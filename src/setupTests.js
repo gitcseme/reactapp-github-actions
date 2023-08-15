35a0c63ea8d94391f81d8a64e553a984f7655886
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2023-08-15 21:12:14 +0600

    add test workflow

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,28 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App Component', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const learnReactLink = screen.getByText(/learn react/i);
+    expect(learnReactLink).toBeInTheDocument();
+  });
+
+  test('renders App logo', () => {
+    render(<App />);
+    const appLogo = screen.getByAltText('logo');
+    expect(appLogo).toBeInTheDocument();
+  });
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+});
